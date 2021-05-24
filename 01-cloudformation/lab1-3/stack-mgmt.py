e9c4a295c677cfd32c02b7f0abd5e37426fd93ca
Brandon.Garcia <brandon.garcia@MACUSSTG2440892.local>
2021-05-24 15:41:14 -0500

    isolating args

--- 01-cloudformation/lab1-3/stack-mgmt.py
+++ 01-cloudformation/lab1-3/stack-mgmt.py
@@ -7,24 +7,6 @@ import json
 ## Usage python3 stack-mgmt.py --action delete --stack_name bgarlab13 --template_file template.yml --parameters_file parameters.json
 ## Relies on relative path unless you specify the absolute path, didn't feel like implementing abspath :p
 
-parser = argparse.ArgumentParser(description='Allows for parameterization of execution')
-parser.add_argument('--action', type=str,
-                    help='the action you wish to perform against the stack. apply or delete', required=True)
-
-parser.add_argument('--stack_name', type=str,
-                    help='the stack you wish to perform the action against', required=True)
-
-parser.add_argument('--template_file', type=str,
-                    help='the template you wish to deploy', required=True)
-
-parser.add_argument('--parameters_file', type=str,
-                    help='the parameters you wish to specify', required=True)
-
-parser.add_argument('--capability', type=str,
-                    help='the template you wish to deploy', required=False, default="CAPABILITY_NAMED_IAM")
-
-args = parser.parse_args()
-
 def apply_stack(client, template_body, stack_name, parameters, capability) -> None:
     if _stack_exists(client, stack_name):
         response = client.update_stack(
@@ -59,6 +41,26 @@ def _init():
     global template
     global regions
     global parameters
+    global args
+
+    parser = argparse.ArgumentParser(description='Allows for parameterization of execution')
+
+    parser.add_argument('--action', type=str,
+                        help='the action you wish to perform against the stack. apply or delete', required=True)
+
+    parser.add_argument('--stack_name', type=str,
+                        help='the stack you wish to perform the action against', required=True)
+
+    parser.add_argument('--template_file', type=str,
+                        help='the template you wish to deploy', required=True)
+
+    parser.add_argument('--parameters_file', type=str,
+                        help='the parameters you wish to specify', required=True)
+
+    parser.add_argument('--capability', type=str,
+                        help='the template you wish to deploy', required=False, default="CAPABILITY_NAMED_IAM")
+
+    args = parser.parse_args()
 
     with open(args.template_file) as f:
         template = f.read()
