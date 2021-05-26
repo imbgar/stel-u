b45093036c295a2d64eedd720860f5caea6d2c10
Brandon.Garcia <brandon.garcia@MACUSSTG2440892.local>
2021-05-26 08:57:17 -0500

    Make assume-role.sh take param of role

--- 03-iam/assume-role.sh
+++ 03-iam/assume-role.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+aws sts assume-role --role-arn arn:aws:iam::324320755747:role/$1 --role-session-name "rolesesh1"  > tmp_creds.json
+
 export AWS_ACCESS_KEY_ID=$(cat tmp_creds.json | jq -r '.Credentials.AccessKeyId')
 export AWS_SECRET_ACCESS_KEY=$(cat tmp_creds.json | jq -r '.Credentials.SecretAccessKey')
 export AWS_SESSION_TOKEN=$(cat tmp_creds.json | jq -r '.Credentials.SessionToken')
